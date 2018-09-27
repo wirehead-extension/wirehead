96dac98b9d1b9ccb7b175c08144725ded1c4e6fb
cboetto <cmitchellboetto@gmail.com>
2018-09-27 17:04:21 -0500

    actually fixed merge conflicts

--- client/dashboard.js
+++ client/dashboard.js
@@ -6,12 +6,6 @@ import history from './history'
 import store from './store'
 import App from './app'
 
-<<<<<<< HEAD:client/dashboard.js
-=======
-// establishes socket connection
-// import './socket'
->>>>>>> c7b60d1992f74221cf23ee3b33c660819d6eb247:client/index.js
-
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
