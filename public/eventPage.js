d26086664cccea66fcc43da80b28a36bbbc637ab
Minkyu Yang <parigu2@msn.com>
2018-10-01 13:14:20 -0500

    time-usage

--- public/eventPage.js
+++ public/eventPage.js
@@ -1,73 +1,224 @@
 chrome.storage.sync.set({
-  timeHistory:[]
+  timeHistory:[],
+  timeEnded: [],
+  totalTime: [{
+    url: undefined,
+    totalTimeConsume: 0,
+  }],
 })
 
 chrome.tabs.onActivated.addListener(
   function(activeInfo) {
     var newDate = new Date();
 
-    var dateString = "Today's date is: ";
+    var dateString = dateConverter(newDate)
 
-    dateString += (newDate.getMonth() + 1) + "/";
-    dateString += newDate.getDate() + "/";
-    dateString += newDate.getFullYear() + " ";
-    dateString += newDate.getHours() + ":";
-    dateString += newDate.getMinutes() + ":";
-    dateString += newDate.getSeconds();
-
-    var current = newDate.getSeconds() + newDate.getMinutes() * 60 + newDate.getHours() * 3600 + newDate.getDate() * 86400
+    var current = timeInSecond(newDate)
 
     chrome.tabs.get(activeInfo.tabId, function(tab) {
-      var mainUrl = ""
-      if(tab.url.indexOf(".com") > -1) {
-        mainUrl = tab.url.slice(0, tab.url.indexOf(".com") + 4)
-      } else if(tab.url.indexOf(".org") > -1) {
-        mainUrl = tab.url.slice(0, tab.url.indexOf(".org") + 4)
-      } else if(tab.url.indexOf(".io") > -1) {
-        mainUrl = tab.url.slice(0, tab.url.indexOf(".io") + 3)
-      } else if(tab.url.indexOf(".net") > -1) {
-        mainUrl = tab.url.slice(0, tab.url.indexOf(".net") + 4)
-      } else {
-        mainUrl = tab.url
-      }
+      var mainUrl = urlCutter(tab.url)
+
       chrome.storage.sync.get(datas=> {
+
         chrome.storage.sync.set({
           currentTabId: activeInfo.tabId,
+          currentTabTime: current,
+          currentTabUrl: mainUrl,
           timeHistory: [
             ...datas.timeHistory, {
               tabId: activeInfo.tabId,
+              title: tab.title,
               url: mainUrl,
               time: dateString,
               timeCal: current,
-              timeEnd: 0
-            }].sort((a,b) => {
-              return a.timeCal < b.timeCal
-            })
+            }],
+            // .sort((a,b) => {
+            //   return a.timeCal < b.timeCal
+            // }),
+          timeEnded: [...datas.timeEnded],
+          totalTime: [...datas.totalTime]
           });
         })
+
       })
+  timerEnding(current);
   }
 )
 
-chrome.tabs.onRemoved.addListener(
-  function(removeInfo) {
-    var endDate = new Date();
+function timerEnding(initialTab) {
+  var newDate = new Date();
+  var current = timeInSecond(newDate)
 
-    var currentEnd = endDate.getSeconds() + endDate.getMinutes() * 60 + endDate.getHours() * 3600 + endDate.getDate() * 86400
+  var dateString = dateConverter(newDate)
 
-    chrome.storage.sync.get(datas => {
-      var afterClosedTab = datas.timeHistory.map(data => {
-        if (data.tabId === removeInfo) {
-          data.timeEnd = (currentEnd - data.timeCal)
-          return data
-        } else {
-          return data
+  chrome.storage.sync.get(datas=>{
+    chrome.tabs.query({active: false}, tabs=>{
+      tabs.forEach(tab=>{
+        if(tab.id === datas.currentTabId) {
+          var timeInfo
+          datas.timeHistory.forEach(data => {
+            if(data.tabId === tab.id) {
+              timeInfo = data.timeCal
+            }
+          })
+
+          var mainUrl = urlCutter(tab.url)
+
+          var newValue = {
+            id: tab.id,
+            title: tab.title,
+            url: mainUrl,
+            time: dateString,
+            timeCal: (current-(timeInfo || initialTab))
+          }
+          chrome.storage.sync.set({
+            timeEnded: [...datas.timeEnded, newValue],
+            totalTime: [...datas.totalTime]
+          })
+          timeAddUp(newValue);
         }
       })
-      alert(afterClosedTab[0].timeEnd)
-      chrome.storage.sync.set({
-        afterClosedTab
+    })
+  })
+}
+
+function timeAddUp(addTime) {
+  chrome.storage.sync.get(datas=>{
+    var calculatedTime = {url: addTime.url,
+      totalTimeConsume: addTime.timeCal}
+    datas.totalTime.forEach(data=>{
+      if (data.url === addTime.url) {
+        calculatedTime = {
+          url: addTime.url,
+          totalTimeConsume: (data.totalTimeConsume + addTime.timeCal)
+        }
+      }
+    })
+    var addedTime = datas.totalTime.filter(data => {
+      return (data.url !== addTime.url && data.url)
+    })
+    chrome.storage.sync.set({
+      timeEnded: [...datas.timeEnded],
+      totalTime: [...addedTime, calculatedTime].sort((a,b)=>{
+        return a.totalTimeConsume > b.totalTimeConsume
       })
     })
+  })
+}
+
+function urlCutter(url) {
+  var mainUrl = ""
+    if(url.indexOf(".com") > -1) {
+      mainUrl = url.slice(0, url.indexOf(".com") + 4)
+    } else if(url.indexOf(".org") > -1) {
+      mainUrl = url.slice(0, url.indexOf(".org") + 4)
+    } else if(url.indexOf(".io") > -1) {
+      mainUrl = url.slice(0, url.indexOf(".io") + 3)
+    } else if(url.indexOf(".net") > -1) {
+      mainUrl = url.slice(0, url.indexOf(".net") + 4)
+    } else {
+      mainUrl = url
+    }
+  return mainUrl
+}
+
+function dateConverter(newDate) {
+  var dateString = "Today's date is: ";
+
+  dateString += (newDate.getMonth() + 1) + "/";
+  dateString += newDate.getDate() + "/";
+  dateString += newDate.getFullYear() + " ";
+  dateString += newDate.getHours() + ":";
+  dateString += newDate.getMinutes() + ":";
+  dateString += newDate.getSeconds();
+
+  return dateString
+}
+
+function timeInSecond(newDate) {
+  return newDate.getSeconds() + newDate.getMinutes() * 60 + newDate.getHours() * 3600 + newDate.getDate() * 86400
+}
+
+chrome.tabs.onUpdated.addListener(
+  function(tabId, changeInfo) {
+    // var mainUrl = ""
+    // if(changeInfo.url.indexOf(".com") > -1) {
+    //   mainUrl = changeInfo.url.slice(0, changeInfo.url.indexOf(".com") + 4)
+    // } else if(changeInfo.url.indexOf(".org") > -1) {
+    //   mainUrl = changeInfo.url.slice(0, changeInfo.url.indexOf(".org") + 4)
+    // } else if(changeInfo.url.indexOf(".io") > -1) {
+    //   mainUrl = changeInfo.url.slice(0, changeInfo.url.indexOf(".io") + 3)
+    // } else if(changeInfo.url.indexOf(".net") > -1) {
+    //   mainUrl = changeInfo.url.slice(0, changeInfo.url.indexOf(".net") + 4)
+    // } else {
+    //   mainUrl = changeInfo.url
+    // }
+    chrome.storage.sync.get(datas=>{
+      // if(datas.currentTabUrl !== mainUrl) {
+        // alert(datas.currentTabUrl)
+      // }
+    })
+})
+
+chrome.tabs.onCreated.addListener(
+  function(tab) {
+    // alert(tab.title)
+    var newDate = new Date();
+    var current = timeInSecond(newDate)
+    var dateString = dateConverter(newDate)
+    var mainUrl = urlCutter(tab.url)
+
+    chrome.storage.sync.get(datas=> {
+
+      chrome.storage.sync.set({
+        currentTabId: tab.tabId,
+        currentTabTime: current,
+        currentTabUrl: mainUrl,
+        timeHistory: [
+          ...datas.timeHistory, {
+            tabId: tab.tabId,
+            title: tab.title,
+            url: mainUrl,
+            time: dateString,
+            timeCal: current,
+          }],
+        timeEnded: [...datas.timeEnded],
+        totalTime: [...datas.totalTime]
+        });
+      })
   }
 )
+
+// chrome.tabs.onRemoved.addListener(
+//   function(removeInfo) {
+//     alert(removeInfo.tabId)
+//     // chrome.tabs.get(removeInfo.tabId, function(tab) {
+//     //   chrome.storage.sync.get(datas=>{
+//     //     alert(removeInfo.url)
+//     //     chrome.storage.sync.set({
+//     //       ...datas
+//     //     })
+//     //   })
+//     // })
+//   }
+
+//   // function(removeInfo) {
+//   //   var endDate = new Date();
+
+//   //   var currentEnd = endDate.getSeconds() + endDate.getMinutes() * 60 + endDate.getHours() * 3600 + endDate.getDate() * 86400
+
+//   //   chrome.storage.sync.get(['timeHistory'],(datas) => {
+//   //     chrome.storage.sync.set({
+//   //       timeHistory: datas.timeHistory.map(data => {
+//   //         if (data.tabId === removeInfo) {
+//   //           timeEnd = (currentEnd - data.timeCal)
+//   //           return {...data, timeEnd}
+//   //         } else {
+//   //           return data
+//   //         }
+//   //       })
+//   //     })
+//   //   })
+//   // }
+// )
+
--- public/index.js
+++ public/index.js
@@ -13,34 +13,60 @@ dateString += newDate.getSeconds();
 chrome.storage.sync.get(function (datas) {
 
   var currentInfo
-  datas.timeHistory.forEach(data => {
-    if(data.tabId === datas.currentTabId) {
-      currentInfo = data.time
-    }
-  })
+  if (datas.timeHistory[0]) {
+    datas.timeHistory.forEach(data => {
+      if(data.tabId === datas.currentTabId) {
+        currentInfo = {
+          title: data.title,
+          url: data.url
+        }
+      }
+    })
+    datas.totalTime.forEach(data => {
+      if(data.url === currentInfo.url) {
+        currentInfo = {...currentInfo,
+          current: (data.totalTimeConsume + current - datas.currentTabTime)
+        }
+      }
+    })
+    document.querySelector('#current').innerText = currentInfo.title + " " + currentInfo.current + " SEC" + "///////"
+  }
 
-  document.querySelector('#current').innerText = currentInfo + "///////"
 
-  datas.timeHistory.map((data, idx) => {
-    // if (data.timeEnd > 0) {
-    //   alert(data.timeEnd)
-    // }
+  datas.timeEnded.map((data, idx) => {
     if (idx < 10) {
       var newDiv = document.createElement('div')
       var objectDiv = document.querySelector('#usage')
+      var timeUsage
       objectDiv.insertBefore(newDiv, objectDiv.firstChild)
-      if (current-data.timeCal < 60) {
-        timeUsage = current-data.timeCal + ' sec'
-      } else if (current-data.timeCal >= 60 && current-data.timeCal < 3600) {
-        timeUsage = Math.floor((current-data.timeCal)/60) + ' min'
-      } else if (current-data.timeCal >= 3600 && current-data.timeCal < 86400) {
-        timeUsage = Math.floor((current-data.timeCal)/3600) + ' hrs' + Math.floor(((current-data.timeCal) % 3600)/60) + 'min'
+      if (data.timeCal < 60) {
+        timeUsage = data.timeCal + ' sec'
+      } else if (data.timeCal >= 60 && data.timeCal < 3600) {
+        timeUsage = Math.floor((data.timeCal)/60) + ' min'
+      } else if (data.timeCal >= 3600 && data.timeCal < 86400) {
+        timeUsage = Math.floor((data.timeCal)/3600) + ' hrs' + Math.floor(((data.timeCal) % 3600)/60) + 'min'
       }
 
       newDiv.appendChild(document.createTextNode(idx+1))
+      newDiv.appendChild(document.createTextNode(data.title))
       newDiv.appendChild(document.createTextNode(data.url + " /"))
       newDiv.appendChild(document.createTextNode(data.time + " " + dateString + " /"))
-      newDiv.appendChild(document.createTextNode(timeUsage))
+      newDiv.appendChild(document.createTextNode(timeUsage + " /"))
+      // alert(datas.currentTabId)
+      // newDiv.appendChild(document.createTextNode(datas.timeEnded[2].time + "///time end data///"))
+      // if(data.timeEnd) {
+      //   newDiv.appendChild(document.createTextNode(data.timeEnd))
+      // } else {
+      //   newDiv.appendChild(document.createTextNode("not closed"))
+      // }
     }
+  })
+  datas.totalTime.map((data, idx) => {
+    var newDiv = document.createElement('div')
+    var objectDiv = document.querySelector('#list')
+    objectDiv.insertBefore(newDiv, objectDiv.firstChild)
+    newDiv.appendChild(document.createTextNode(idx+1))
+    newDiv.appendChild(document.createTextNode(data.url + " /"))
+    newDiv.appendChild(document.createTextNode(data.totalTimeConsume + " sec"))
   })
 })
