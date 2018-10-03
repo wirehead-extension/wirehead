66858d52412a95c505f3b3b5dc091e111f78f18d
dustinheestand <heestand@post.harvard.edu>
2018-10-03 18:04:40 -0500

    seed script creates plausible history data

--- client/db.js
+++ client/db.js
@@ -2,7 +2,7 @@ import Dexie from 'dexie'
 
 const db = new Dexie('wirehead')
 db.version(5).stores({
-  history: '++id, url, origin, start',
+  history: '++id, url, start, end, time, label',
   summaryHistory: 'url'
 })
 export default db
