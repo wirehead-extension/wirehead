6dc1c536db22c5118b12318de29eb7502c7d8f4f
dustinheestand <heestand@post.harvard.edu>
2018-10-04 00:15:54 -0500

    more instructions"

--- client/db.js
+++ client/db.js
@@ -1,5 +1,7 @@
+//uncomment the commented below to seed the db!
+//make sure to re-comment, re-build, and reload into Chrome after you seed!
 import Dexie from 'dexie'
-import history from '../script/seed'
+//import history from '../script/seed'
 
 const db = new Dexie('wirehead')
 db.version(5).stores({
