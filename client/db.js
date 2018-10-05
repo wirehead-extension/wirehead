cc9078c20363ff82ef0d6ab3f00c6e2a466778c3
dustinheestand <heestand@post.harvard.edu>
2018-10-05 16:13:10 -0500

    made user flow to prompt user to learn if there's not a model

--- client/db.js
+++ client/db.js
@@ -10,6 +10,37 @@ db.version(5).stores({
   bayesModel: '++id, model'
 })
 
+function makeNotification() {
+  chrome.notifications.create('dashboard.html#about', {
+    type: 'basic',
+    title: 'Thanks for downloading Wirehead!',
+    iconUrl: 'gray.png',
+    message: 'Learn more about our app!',
+    requireInteraction: true
+  })
+
+  chrome.notifications.onClicked.addListener(function handleClick(
+    notificationId
+  ) {
+    chrome.tabs.create({url: notificationId})
+    chrome.notifications.onButtonClicked.removeListener(handleClick)
+  })
+}
+function initNotification() {
+  //If there's an active page, get the page title and init a notification
+  chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    if (tabs[0]) {
+      makeNotification(tabs[0].title)
+    }
+  })
+}
+
+db.bayesModel.get(100000, m => {
+  if (!m) {
+    initNotification()
+  }
+})
+
 db.history.get(1, s => {
   if (!s) {
     db.history.bulkAdd(history)
