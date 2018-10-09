32fddff638ccc967d30cc75de651efb6c96e396f
Kevin Gislason <kevingislason@gmail.com>
2018-10-09 15:00:49 -0500

    options form disappears after submit, tweaked dash navbar

--- client/dashboard/components/appConfig.js
+++ client/dashboard/components/appConfig.js
@@ -20,16 +20,19 @@ class AppConfig extends React.Component {
   }
 
   async componentDidMount() {
+    console.log('comp mounted')
     const currentOptions = await getOptions()
+    console.log('options in comp did mount', currentOptions)
     this.setState(currentOptions)
   }
 
   handleSubmit = async event => {
     event.preventDefault()
     await updateOptions(this.state)
-    //dispatch signal to chrome runtime!!!!
-    // chrome.runtime.sendMessage('lorem ipsum dolor sit amet')
-    // console.log('I guess nothing went wrong?')
+    await this.setState({
+      trainingPopupFrequency: null,
+      allowShaming: null
+    })
   }
 
   handleChangePopups = (e, {value}) =>
@@ -37,63 +40,75 @@ class AppConfig extends React.Component {
   handleChangeShaming = (e, {value}) => this.setState({allowShaming: value})
 
   render() {
-    console.log(this.state)
+    console.log('rendered')
+    console.log('this.state', this.state)
+    console.log('condition?', Object.keys(this.state).length)
+    console.log('keys', Object.keys(this.state))
     let trainingPopupFrequency = this.state.trainingPopupFrequency
     let allowShaming = this.state.allowShaming
     return (
-      <Container>
-        <Header as="h2">Options</Header>
-        <Divider hidden />
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Group grouped>
-            Can Wirehead shame you with occasional notifications if you're
-            wasting time?
-            <Form.Field
-              label="Yes"
-              control={Radio}
-              type="radio"
-              value={true}
-              checked={allowShaming === true}
-              onChange={this.handleChangeShaming}
-            />
-            <Form.Field
-              label="No"
-              control={Radio}
-              type="radio"
-              value={false}
-              checked={allowShaming === false}
-              onChange={this.handleChangeShaming}
-            />
-          </Form.Group>
-          <Form.Group grouped>
-            Can the Wirehead AI prompt you to train it?
-            <Form.Field
-              label="Yes"
-              control={Radio}
-              type="radio"
-              value="normal"
-              checked={trainingPopupFrequency === 'normal'}
-              onChange={this.handleChangePopups}
-            />
-            <Form.Field
-              label="Occasionally"
-              control={Radio}
-              type="radio"
-              value="low"
-              checked={trainingPopupFrequency === 'low'}
-              onChange={this.handleChangePopups}
-            />
-            <Form.Field
-              label="No (not recommended for new users!)"
-              control={Radio}
-              type="radio"
-              value="never"
-              checked={trainingPopupFrequency === 'never'}
-              onChange={this.handleChangePopups}
-            />
-          </Form.Group>
-          <Button type="submit">Update</Button>
-        </Form>
+      <Container text>
+        {/* Don't render after submit */}
+        {allowShaming !== null && trainingPopupFrequency !== null ? (
+          <Container>
+            <Header as="h2">Options</Header>
+            <Divider />
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Group grouped>
+                Can Wirehead shame you with occasional notifications if you're
+                wasting time?
+                <Form.Field
+                  label="Yes"
+                  control={Radio}
+                  type="radio"
+                  value={true}
+                  checked={allowShaming === true}
+                  onChange={this.handleChangeShaming}
+                />
+                <Form.Field
+                  label="No"
+                  control={Radio}
+                  type="radio"
+                  value={false}
+                  checked={allowShaming === false}
+                  onChange={this.handleChangeShaming}
+                />
+              </Form.Group>
+              <Divider />
+              <Form.Group grouped>
+                Can the Wirehead AI prompt you to train it?
+                <Form.Field
+                  label="Yes"
+                  control={Radio}
+                  type="radio"
+                  value="normal"
+                  checked={trainingPopupFrequency === 'normal'}
+                  onChange={this.handleChangePopups}
+                />
+                <Form.Field
+                  label="Occasionally"
+                  control={Radio}
+                  type="radio"
+                  value="low"
+                  checked={trainingPopupFrequency === 'low'}
+                  onChange={this.handleChangePopups}
+                />
+                <Form.Field
+                  label="No (not recommended for new users!)"
+                  control={Radio}
+                  type="radio"
+                  value="never"
+                  checked={trainingPopupFrequency === 'never'}
+                  onChange={this.handleChangePopups}
+                />
+              </Form.Group>
+              <Divider />
+              <Button type="submit">Update</Button>
+            </Form>
+          </Container>
+        ) : (
+          <p>Options updated </p>
+        )}
       </Container>
     )
   }
